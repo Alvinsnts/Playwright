3c2014439ac7afbdccedf54ffa9e95ac013c376f
alvinsnts <alvinsantoso0123@gmail.com>
2021-10-27 13:32:22 +0700

    commit copy paste password

--- pages/Login/auth.ts
+++ pages/Login/auth.ts
@@ -22,4 +22,23 @@ export class AuthPage {
         await this.page.click('text=Logout');
         await this.page.click('a:has-text("Ya")');
     }
+    async loginUsername(email: string){
+        await this.page.type('[placeholder="Email or Username"]', email);
+    }
+    async loginPassword(password: string){
+        await this.page.type('[placeholder="Password"]', password);
+    }
+    async loginButton(){
+        await this.page.click('text=Login');
+    }
+    async usernameBox(){
+        await this.page.hover('#email');
+        await this.page.click('#email');
+    }
+    async passwordBox(){
+        await this.page.hover('#password');
+        await this.page.click('#password');
+    }
+    
+    
 }
--- test/Login/login.spec.ts
+++ test/Login/login.spec.ts
@@ -9,3 +9,13 @@ test.skip('Verify user can login with correct email and password', async ({ page
   await expect(page).toHaveURL('https://pen-stage.udata.id/page/view/home');
 });
 
+test.only('2.1.8 Verify paste functionality in password field', async ({ page }) => {
+  const loginPage = new AuthPage(page);
+  await loginPage.open();
+  await loginPage.loginUsername('dev');
+  await loginPage.usernameBox();
+  await page.keyboard.press('Control+A');
+  await page.keyboard.press('Control+C');
+  await loginPage.passwordBox();
+  await page.keyboard.press('Control+V');
+});
